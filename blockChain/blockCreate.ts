ef2f078db467ac6fe65496c32202de0bd10bd162
Bereket3 <workumyfather@gmail.com>
2024-10-09 07:49:07 +0300

    add new block

--- blockChain/blockCreate.ts
+++ blockChain/blockCreate.ts
@@ -1,14 +1,7 @@
-import { Block, addBlockToChain } from "./block"
+import { generateNextBlock } from "./block"
 
-for (let i = 1; i < 5; i++) {
-  const newBlock = new Block(
-    i + 1,
-    '91a73664bc84c0baa1fc75ea6e4aa6d1d20c5df664c724e3159aefc2e1186627',
-    '',
-    1465154705,
-    'my block',
-    0,
-    0
-  )
-  addBlockToChain(newBlock)
+for (let i = 0; i < 3; i++) {
+  generateNextBlock(
+    `very important data ${i}`
+  ); 
 }
